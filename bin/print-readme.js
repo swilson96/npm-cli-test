ccbf884b68aba76772fa25c2ef552af18a9228d0
Simon Wilson <simon@yulife.com>
2019-02-11 20:41:45 +0000

    tidyup and try for importing project's readme

--- bin/print-readme.js
+++ bin/print-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
+
 const fs = require("fs");
 const path = require("path");
-const CHANGELOG = path.join(__dirname, "..", "README.md");
+const README = "README.md";
 
 (function main() {
-  fs.readFile(CHANGELOG, "utf8", (err, data) => {
+  fs.readFile(README, "utf8", (err, data) => {
     if (err) {
       throw err;
     }
